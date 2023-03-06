b534b64c846995eab9ccf91621ccc9570c720f4c
ljx360 <79862310+ljx360@users.noreply.github.com>
2023-03-06 22:13:05 +0800

    Update diy_ljx_append.sh

--- diy_ljx_append.sh
+++ diy_ljx_append.sh
@@ -1,3 +1,21 @@
-for line in `cat /workdir/openwrt/config_ext`;do
-  echo $line>>/workdir/openwrt/.config
+file1=/workdir/ljx/config_ext
+file2=/workdir/openwrt/.config
+
+
+
+if [ -f $file1 ];then
+
+if [ ! -f $file2 ];then
+echo "touch $file2"
+touch $file2
+fi
+
+for line in `cat $file1`;do
+echo $line>>$file2
 done
+
+else
+
+echo "miss $file1"
+
+fi
