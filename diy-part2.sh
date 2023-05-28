7205d45f88af33090f59d95fc10540403299ead0
ljx360 <79862310+ljx360@users.noreply.github.com>
2023-05-28 21:28:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+#ljx
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
